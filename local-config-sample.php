0224bdca611005652c611aa84f5b0b5f99b1220b
Barry Kooij <git@barrykooij.nl>
2013-11-21 10:41:30 +0100

    Local config update

--- local-config-sample.php
+++ local-config-sample.php
@@ -1,12 +1,19 @@
 <?php
-/*
-This is a sample local-config.php file
-In it, you *must* include the four main database defines
-
-You may include other settings here that you only want enabled on your local development checkouts
-*/
 
 define( 'DB_NAME', 'local_db_name' );
 define( 'DB_USER', 'local_db_user' );
 define( 'DB_PASSWORD', 'local_db_password' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
+
+define( 'WP_DEBUG', true );
+if( WP_DEBUG ) {
+	define( 'SCRIPT_DEBUG', true );
+	define( 'WP_DEBUG_LOG', true );
+	define( 'WP_DEBUG_DISPLAY', false );
+	@ini_set( 'display_errors', 0 );
+}
+
+define( 'WP_CACHE', false );
